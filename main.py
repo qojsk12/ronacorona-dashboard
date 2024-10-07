739ac78e1571cc5619575297e421799a842ddcfe
kjs <qojsk12@naver.com>
2024-10-07 13:16:26 +0900

    ad dashboard

---
+++ main.py
@@ -0,0 +1,27 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+stylesheets = [
+    "https://cdn.jsdelivr.net/npm/reset-css@5.0.2/reset.min.css",
+    "https://fonts.googleapis.com/css2?family=Open+Sans:ital@0;1&display=swap"
+]
+
+app = Dash(__name__, external_stylesheets=stylesheets)
+
+app.layout = html.Div(
+    style={
+        'minHeight': '100vh',
+        'backgroundColor': '#111111',
+        'color': 'white',
+        'fontFamily': 'Open Sans, sans-serif'
+    },
+    children=[
+        html.Header(
+            style={'textAlign': 'center', 'paddingTop': '50px'},
+            children=[html.H1("Corona Dashboard", style={'fontSize': '40px'})])
+    ],
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
